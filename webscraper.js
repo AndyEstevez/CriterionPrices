53749b8e63622bcadcaa07bdde78b8a75a65a5b3
unknown <andyestevez27@gmail.com>
2021-08-15 21:43:05 -0400

    added webscraper

---
+++ webscraper.js
@@ -0,0 +1,58 @@
+const puppeteer = require('puppeteer');
+
+async function scrapeAllTitles(url) {
+
+    // start browser and create 2 pages
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    const page2 = await browser.newPage();
+
+    const title_details = await page.evaluate(() => {
+        // get the table
+        let table = document.querySelector('#gridview > tbody')
+        let panels = Array.from(table.children);
+
+        // iterates through each table row for Criterion title
+        // gets all basic info for each title
+        let title_info = panels.map(panel => {
+            let spineNumber = panel.querySelector('.g-spine').innerText;
+            let img = panel.querySelector('.g-img').querySelector('img').src;
+            let title = panel.querySelector('.g-title').innerText;
+            let director = panel.querySelector('.g-director').innerText;
+            let country = panel.querySelector('.g-country').innerText;
+            let year = panel.querySelector('.g-year').innerText;
+            let link = panel.getAttribute('data-href');
+
+            return { spineNumber, img, title, director, country, year, link };
+        })
+
+        return title_info;
+
+    })
+
+    // iterate through each link from the array 
+    // because don't have price on all collection page on criterion.com
+    for(let t of title_details){
+        let link = t.link
+        page2.waitForTimeout(200);
+
+        await page2.goto(link)
+        const price = await page2.$eval('body > div.page-contain > main > article.product__details > div.content-container.product-primary-content-container > div > div.right > div > div > section.purchase-options.pk-c-purchase-options > form > fieldset.purchase-buttons > div:nth-child(1) > label > span.meta-prices > span.item-price', 
+                                        el => el.innerText)
+        t['price'] = price
+    }
+    await page2.close();
+
+    console.log(title_details)
+
+    return title_details;
+    
+}
+
+// using temp url for testing
+// scrapePage('https://www.criterion.com/shop/browse/list?format=blu-ray&decade=2000s&country=United%20States')
+
+module.exports = {
+    scrapeAllTitles
+}
